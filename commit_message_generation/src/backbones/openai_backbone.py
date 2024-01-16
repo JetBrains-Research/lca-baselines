f754cb9e954e28a9721c114fba53d2bb765c8b70
Alexandra Eliseeva <saridormi123@gmail.com>
2024-01-16 12:56:38 +0100

    Add backoff on exceptions from OpenAI API & make pip requirements up-to-date

--- commit_message_generation/src/backbones/openai_backbone.py
+++ commit_message_generation/src/backbones/openai_backbone.py
@@ -1,9 +1,11 @@
 import os
-from typing import Any, Dict, Optional
+from typing import Any, Dict, List, Optional
 
+import backoff
 import openai
 
 from src.prompts import CMGPrompt
+from src.utils import ChatMessage
 
 from .base_backbone import CMGBackbone
 
@@ -23,9 +25,13 @@ class OpenAIBackbone(CMGBackbone):
         self._prompt = prompt
         self._parameters = parameters
 
+    @backoff.on_exception(backoff.expo, openai.APIError)
+    def _get_chat_completion(self, messages: List[ChatMessage]) -> openai.types.chat.ChatCompletion:
+        return self._client.chat.completions.create(messages=messages, model=self._model_name, **self._parameters)  # type: ignore[arg-type]
+
     def generate_msg(self, preprocessed_commit_mods: str, **kwargs) -> Dict[str, Optional[str]]:
         prompt = self._prompt.chat(preprocessed_commit_mods)
-        response = self._client.chat.completions.create(messages=prompt, model=self._model_name, **self._parameters)  # type: ignore[arg-type]
+        response = self._get_chat_completion(messages=prompt)
         assert response.choices[0].message.content, "Empty content in OpenAI API response."
         return {
             "prediction": response.choices[0].message.content,
