91fa290df8508e36f54bcf03c9287c9c066b37b2
Maria.Tigina <maria.tigina@jetbrains.com>
2024-02-13 18:14:27 +0100

    Fix score metrics

--- bug_localization/baselines/metrics/classification_metrics.py
+++ bug_localization/baselines/metrics/classification_metrics.py
@@ -3,7 +3,7 @@ import sklearn
 
 
 def pr_auc_score(y_true: np.ndarray[int], y_pred: np.ndarray[float]) -> float:
-    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_true, y_pred, pos_label=2)
+    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_true, y_pred)
     return sklearn.metrics.auc(fpr, tpr)
 
 
@@ -12,7 +12,7 @@ def roc_auc_score(y_true: np.ndarray[int], y_pred: np.ndarray[float]) -> float:
 
 
 def f1_score(y_true: np.ndarray[int], y_pred: np.ndarray[float]) -> tuple[float, float]:
-    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_true, y_pred, pos_label=2)
+    fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_true, y_pred)
     f1_scores = 2 * tpr * fpr / (tpr + fpr)
     best_f1 = np.max(f1_scores)
     best_thresh = thresholds[np.argmax(f1_scores)]
